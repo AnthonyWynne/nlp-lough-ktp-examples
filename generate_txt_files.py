8b60581627a81a7579cecd1ca7e7395cf1407994
Anthony Wynne <a.wynne@svgc.co.uk>
2023-04-14 17:21:09 +0100

    first run

---
+++ generate_txt_files.py
@@ -0,0 +1,46 @@
+# %%
+import sqlalchemy
+import re
+
+# Create engine: engine
+engine = sqlalchemy.create_engine('sqlite:///./data/text.db')
+
+# query to get the articles ids
+query = "SELECT pageId FROM articles"
+
+
+def clean_text(text):
+    """strip punctuation, spaces and non printing
+    character from the text to format for a filename
+
+    Args:
+        text (string): the input text
+
+    Returns:
+        string: lowercase text with no spaces or punctuation
+    """
+    text = re.sub(r'[^\w\s]', '', text).lower()
+    text = re.sub(r'\s+', '_', text)
+    return text.strip()
+
+
+# get the articles ids
+articles_ids = engine.execute(query).fetchall()
+
+# query to get the articles
+articles_limit = 100
+for i, article_id in enumerate(articles_ids):
+    query = f"SELECT text FROM articles WHERE pageId = {article_id[0]}"
+    article = engine.execute(query).fetchone()
+    # get the article title
+    query = f"SELECT title FROM articles WHERE pageId = {article_id[0]}"
+    title = engine.execute(query).fetchone()
+    # clean the title
+    filename = clean_text(title[0])
+    # write the article to a file
+    with open(f"./data/{filename}.txt", "w") as file:
+        file.write(article[0])
+    if i == articles_limit:
+        break
+
+# %%
