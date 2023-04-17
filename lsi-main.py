08cc143800726605934e5cc899f342e49df8154f
Anthony Wynne <a.wynne@svgc.co.uk>
2023-04-17 10:58:53 +0100

    funtions tidy up

--- lsi-main.py
+++ lsi-main.py
@@ -8,69 +8,164 @@ from sklearn.decomposition import TruncatedSVD
 from sklearn.metrics.pairwise import cosine_similarity
 from string import punctuation
 from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
 nltk.download('stopwords')
 
 data_path = "./data/"
 files = glob.glob(f"{data_path}*.txt")
+assert files, "No files found in data folder"
 print(f"The first 5 of {len(files)}", files[:5])
 
 
-def add_doc_to_vocab(filename, vocab):
-    doc = load_doc(filename)
-    tokens = clean_doc(doc)
-    vocab.update(tokens)
-
-
 def load_doc(filename):
+    """Load a doc file into memory
+
+    Args:
+        filename (str): path to the file
+
+    Returns:    
+        str: the content of the file
+
+    """
     with open(filename, 'r') as file:
         text = file.read()
     return text
 
 
+def remove_stopwords(tokens):
+    """Remove stopwords from a list of tokens
+    Args:
+        tokens (list): list of tokens
+     
+    Returns:
+    list: list of tokens without stopwords
+    """
+    stop_words = set(stopwords.words('english'))
+    tokens = [w for w in tokens if w not in stop_words]
+    return tokens
+
+
+def make_trans_table(tokens):
+    """Create a translation table and remove punctuation
+    from a list of tokens
+
+    Args:
+        tokens (list): list of tokens
+    
+    Returns:
+        list: list of tokens without punctuation
+    """
+    table = str.maketrans('', '', punctuation)
+    tokens = [w.translate(table) for w in tokens]
+    return tokens
+
+
+def porter_stem(tokens):
+    """Stem tokens using the Porter Stemming algorithm
+
+    Args:
+        tokens (list): list of tokens
+    
+    Returns:
+        list: list of stemmed tokens
+    """
+    porter = PorterStemmer()
+    tokens = [porter.stem(word) for word in tokens]
+    return tokens
+
+
+def Split_doc(doc):
+    """Split a long document string into
+    basic tokens splitting on whitespace.
+
+    Args:
+        doc (str): the doc to split
+    
+    Returns:
+        list: list of tokens
+    """
+    tokens = doc.split()
+    tokens = [word.lower() for word in tokens]
+    return tokens
+
+
+def clean_doc(doc):
+    """Clean a document string using the
+    a series of functions to split, stem,
+    remove punctuation and stopwords.
+
+    Args:
+        doc (str): the doc to clean
+
+    Returns:
+        list: list of cleaned tokens
+    """
+    tokens = Split_doc(doc)
+    tokens = porter_stem(tokens)
+    tokens = make_trans_table(tokens)
+    tokens = [word for word in tokens if word.isalpha()]
+    tokens = remove_stopwords(tokens)
+    tokens = [word for word in tokens if len(word) > 1]
+    return tokens
+
+
+def add_doc_to_vocab(filename, vocab):
+    """Add all tokens in a doc to the vocab
+    Uses the clean_doc function to clean the doc 
+    before adding to the vocab.
+    Uses the load_doc function to load the doc
+
+    Args:
+        filename (str): path to the file
+        vocab (set): set of all words
+
+    Returns:
+        None
+
+    """
+    doc = load_doc(filename)
+    tokens = clean_doc(doc)
+    vocab.update(tokens)
+
+
 def save_list(lines, filename):
+    """Save a list of lines to file
+    Create a new file and write each line in the list
+    to the file.
+
+    Args:
+        lines (list): list of lines to save
+        filename (str): path to the file
+    
+    Returns:
+        None
+    
+    """
     data = '\n'.join(lines)
     with open(filename, 'w') as file:
         file.write(data)
 
 
-def random_sample(num1, num2):
-    dataList = list(range(num1))
-    TrainIndex = []
-    for _ in range(num2):
-        randIndex = int(random.uniform(0, len(dataList)))
-        TrainIndex.append(dataList[randIndex])
-        del (dataList[randIndex])
-    TestIndex = dataList
-    return TrainIndex, TestIndex
-
-
 def load_doc_lines(filename):
+    """Load all lines in a file into memory
+
+    Args:
+        filename (str): path to the file
+
+    Returns:
+        list: list of lines in the file
+
+    """
     with open(filename, 'rt') as file:
         lines = []
         while 1:
-
             if line := file.readline():
                 lines.append(line.strip("\n"))
             else:
                 break
     return lines
-
-
-def clean_doc(doc):
-    tokens = doc.split()
-    tokens = [word.lower() for word in tokens]
-    from nltk.stem.porter import PorterStemmer
-    porter = PorterStemmer()
-    tokens = [porter.stem(word) for word in tokens]
-    table = str.maketrans('', '', punctuation)
-    tokens = [w.translate(table) for w in tokens]
-    tokens = [word for word in tokens if word.isalpha()]
-    stop_words = set(stopwords.words('english'))
-    tokens = [w for w in tokens if w not in stop_words]
-    tokens = [word for word in tokens if len(word) > 1]
-    return tokens
 
 
 def doc_to_line(doc):
@@ -86,32 +181,23 @@ def process_docs(files):
     return lines
 
 
+def random_sample(num1, num2):
+    dataList = list(range(num1))
+    TrainIndex = []
+    for _ in range(num2):
+        randIndex = int(random.uniform(0, len(dataList)))
+        TrainIndex.append(dataList[randIndex])
+        del (dataList[randIndex])
+    TestIndex = dataList
+    return TrainIndex, TestIndex
+
+
 def prepare_data(train_docs, mode, vocab):
     vectorizer = CountVectorizer(vocabulary=vocab)
     transformer = TfidfTransformer(norm='l2')
     return transformer.fit_transform(vectorizer.fit_transform(train_docs))
 
 
-# ArRe_train_lines = load_doc_lines(f"{data_path}/Reduced_ArtsReviews_5000.txt")
-# %%
-ArRe_train_lines = [load_doc_lines(file)[0] for file in files]
-# %%
-train_docs = process_docs(ArRe_train_lines)
-# %%
-vocab = []
-for ll in train_docs:
-    tt = ll.split()
-    for ww in tt:
-        if ww not in vocab:
-            vocab.append(ww)
-
-Xtrain = prepare_data(train_docs, 'tfidf', vocab)
-
-trunc_SVD_model = TruncatedSVD(n_components=25)
-approx_Xtrain = trunc_SVD_model.fit_transform(Xtrain)
-print(f"Approximated Xtrain shape: {str(approx_Xtrain.shape)}")
-
-
 def preprocess_query(review, mode, vocab):
     tokens = clean_doc(review)
     line = ' '.join(tokens)
@@ -120,35 +206,17 @@ def preprocess_query(review, mode, vocab):
     return transformer.fit_transform(vectorizer.fit_transform([line]))
 
 
-querys = ['The pen is good.', 'The pen is poor.']
-Top_n_reviews = 10
-for query in querys:
-    encoded_query = preprocess_query(query, 'tfidf', vocab)
-    transformed_query = trunc_SVD_model.transform(encoded_query)
-    similarities = cosine_similarity(approx_Xtrain, transformed_query)
-    indexes = np.argsort(similarities.flat)[::-1]
-
-    print('\n' + 'Query: ' + query)
-    for i in range(Top_n_reviews):
-        print(f"Top {str(i + 1)} result:")
-        print(f"Reviews ID: {str(indexes[i])}")
-        print(ArRe_train_lines[indexes[i]])
-
-
-# preprocess query
-def preprocess_query(review, mode, vocab):
-    # clean
-    tokens = clean_doc(review)
-    # convert to line
-    line = ' '.join(tokens)
-    # encode
-    vectorizer = CountVectorizer(vocabulary=vocab)
-    transformer = TfidfTransformer(norm='l2')
-    return transformer.fit_transform(vectorizer.fit_transform([line]))
-
-
-# Interplot Precision for standard Recall
 def InterplotPrecision(p=0.1, Precision=None, Recall=None):
+    """Interplot Precision for standard Recall
+
+    Args:
+        p (float, optional): [description]. Defaults to 0.1.
+        Precision ([type], optional): [description]. Defaults to None.
+        Recall ([type], optional): [description]. Defaults to None.
+
+    Returns:
+        [type]: [description]
+    """
 
     if p >= 1.0:
         p = 0.9
@@ -198,6 +266,37 @@ def compute_R_P_F1(re_mark=None, QuRe_ID=None):
         F1measure.append(F1)
     return Recall, Precision, F1measure
 
+
+# ArRe_train_lines = load_doc_lines(f"{data_path}/Reduced_ArtsReviews_5000.txt")
+
+ArRe_train_lines = [load_doc_lines(file)[0] for file in files]
+train_docs = process_docs(ArRe_train_lines)
+vocab = []
+for ll in train_docs:
+    tt = ll.split()
+    for ww in tt:
+        if ww not in vocab:
+            vocab.append(ww)
+
+Xtrain = prepare_data(train_docs, 'tfidf', vocab)
+
+trunc_SVD_model = TruncatedSVD(n_components=25)
+approx_Xtrain = trunc_SVD_model.fit_transform(Xtrain)
+print(f"Approximated Xtrain shape: {str(approx_Xtrain.shape)}")
+
+querys = ['The pen is good.', 'The pen is poor.']
+Top_n_reviews = 10
+for query in querys:
+    encoded_query = preprocess_query(query, 'tfidf', vocab)
+    transformed_query = trunc_SVD_model.transform(encoded_query)
+    similarities = cosine_similarity(approx_Xtrain, transformed_query)
+    indexes = np.argsort(similarities.flat)[::-1]
+
+    print('\n' + 'Query: ' + query)
+    for i in range(Top_n_reviews):
+        print(f"Top {str(i + 1)} result:")
+        print(f"Reviews ID: {str(indexes[i])}")
+        print(ArRe_train_lines[indexes[i]])
 
 queries = ['The pen is good.', 'The pen is poor.']
 
